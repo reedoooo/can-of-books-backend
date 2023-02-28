2e726e37f4b75b025cefd2fcc15ee4aa28237e3c
reedoooo <readvogt@gmail.com>
2023-02-27 17:29:42 -0800

    lab 1

--- server.js
+++ server.js
@@ -3,16 +3,42 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const axios = require("axios");
+
+// const mongoose = require("mongoose");
+// async function main() {
+//   await mongoose.connect("mongodb://127.0.0.1:27017/test");
+
+//   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+// }
+// main().catch((err) => console.log(err));
+
+function getApi(req, res, next) {
+  let api = req.query.searchQuery;
+  let apiUrl = `${process.env.DATABASE_URL}`;
+
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      let liveInfo = response.data.results
+      res.status(200).send(liveInfo);
+    })
+    .catch((error) => next(error));
+}
+
 
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+// app.get('/test', (request, response) => {
+//   response.send('test request received')
+// })
+app.get("/books", getApi);
 
-  response.send('test request received')
-
-})
+// app.get("/books", (request, response) => {
+//   response.send("books");
+// });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
